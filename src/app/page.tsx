4daa16626e8e418bf063b2c5992835279ab3dbcc
paulorotundaro <paulo.rotundaro@zoss.com.br>
2025-02-07 15:50:39 -0300

    fix: column content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [inputChatId, setInputChatId] = useState("");
+  type Message = {
+    from: string;
+    text: string;
+  };
+
   const [inputSessionId, setInputSessionId] = useState("");
-  const [sessionId, setSessionId] = useState("");
+  const [chatId, setChatId] = useState("");
   const [debounceContent, setDebounceContent] = useState("");
   const [logContent, setLogContent] = useState("");
+  const [botmakerContent, setBotmakerContent] = useState<Message[]>([]);
   const [copied, setCopied] = useState(false);
 
   const handleCopy = async (text: string) => {
@@ -20,55 +25,33 @@ export default function Home() {
     }
   };
 
-  const fetchSessionId = async (chatId: string) => {
-    if (!inputChatId) throw new Error("ID do chat não informado.");
+  const fetchSessionId = async (sessionId: string) => {
+    if (!inputSessionId) throw new Error("ID da sessão não informado.");
 
-    try {
-      const response = await fetch(`https://api.botmaker.com/v2.0/chats/${chatId}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "access-token": "eyJhbGciOiJIUzUxMiJ9.eyJidXNpbmVzc0lkIjoiem9zc21vdmlkYSIsIm5hbWUiOiJBZG1pbiBBUElzIiwiYXBpIjp0cnVlLCJpZCI6ImRZbExYZkx2YjloeHF5WmpaaXZRSkJUMXNONTIiLCJleHAiOjE4NTcwNzE4NzcsImp0aSI6ImRZbExYZkx2YjloeHF5WmpaaXZRSkJUMXNONTIifQ.ee_L_HlLYdYpyg6y2-AyZ6_bRzSCYMOxJZAH4NbQTRNCsYZKSA96tTSFRcZiOGlaiLIWI1-ZYjxhOPGUPlf5iA", // 🔹 Adicionando o cabeçalho de autenticação
-        },
-      });
-      if (!response.ok) throw new Error("Erro ao buscar dados da API");
-
-      const data = await response.json();
-      const sessionIdFromApi = data?.variables?.ia_session_id;
-
-      if (sessionIdFromApi) {
-        setSessionId(sessionIdFromApi);
-
-        const contentResponse = await fetch(`https://api-ia.zoss.com.br/getContent?iaSessionId=${sessionIdFromApi}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-        
-        console.log(`Erro: ${contentResponse.status}`);
-
-        const content = await contentResponse.json();
-        setDebounceContent(content?.debounceContent);
-        setLogContent(content?.logContent);
-
-      } else {
-        throw new Error("ia_session_id não encontrado");
+    const contentResponse = await fetch(`https://api-ia.zoss.com.br/getContent?iaSessionId=${sessionId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
       }
-    } catch (err) {
-      console.error(err);
-      setSessionId("");
+    });
+    if (!contentResponse.ok) {
+      console.log(`Erro: ${contentResponse.status}`);
     }
+
+    setChatId(sessionId.split('_')[0]);
+
+    const content = await contentResponse.json();
+    setDebounceContent(content?.debounceContent);
+    setLogContent(content?.logContent);
+    setBotmakerContent(content?.botmakerContent?.messages);
   };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex">
-        <div className="mr-3 input-title">Chat ID:</div>
-        <input className="pl-1 pr-1 " placeholder="Digite o ID do chat" value={inputChatId} onChange={(e) => setInputChatId(e.target.value)} />
         <div className="mr-3 input-title">Session ID:</div>
         <input className="pl-1 pr-1 " placeholder="Digite o ID da sessão" value={inputSessionId} onChange={(e) => setInputSessionId(e.target.value)} />
-        <button className="btn" onClick={() => fetchSessionId(inputChatId)}>Enviar</button>
+        <button className="btn" onClick={() => fetchSessionId(inputSessionId)}>Enviar</button>
       </div>
       <main className="wdt-100 flex flex-col gap-8">
         <div className="column-titles flex">
@@ -77,9 +60,13 @@ export default function Home() {
           <p className="column-title">LOG</p>
         </div>
         <div className="columns flex gap-4 space-around">
-          <div className="column-30" onClick={() => handleCopy(`https://api.botmaker.com/v2.0/messages/?long-term-search=true&chat-id=${inputChatId}`)}><p>https://api.botmaker.com/v2.0/messages/?long-term-search=true&chat-id={inputChatId}</p></div>
-          <div className="column-30 scrollbar" onClick={() => handleCopy(`https://debounce.ia.zoss.com.br/getObjects?id=${sessionId}`)}>{debounceContent}</div>
-          <div className="column-30 scrollbar" onClick={() => handleCopy(`https://ia-api-log.zoss.com.br/logs?projectId=movida-rac&identifier=${sessionId}`)}>{logContent}</div>
+          <div className="column-30 scrollbar" onClick={() => handleCopy(`https://api.botmaker.com/v2.0/messages/?long-term-search=true&chat-id=${chatId}`)}>{botmakerContent.map(message => (
+            <div className="wdt-100" key={message.from + "_" + message.text}>
+              <p className="wdt-100 mb-5">{message.from}: {message.text}</p>
+            </div>
+          ))}</div>
+          <div className="column-30 scrollbar" onClick={() => handleCopy(`https://debounce.ia.zoss.com.br/getObjects?id=${inputSessionId}`)}>{debounceContent}</div>
+          <div className="column-30 scrollbar" onClick={() => handleCopy(`https://ia-api-log.zoss.com.br/logs?projectId=movida-rac&identifier=${inputSessionId}`)}>{logContent}</div>
         </div>
       </main>
       {copied && <p className="mt-2 copied">Url copiada!</p>}

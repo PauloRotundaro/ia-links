d0e4dda7916d66bece80121733b810709db15cc4
Paulo Rotundaro <paulo.rotundaro@zoss.com.br>
2026-08-13 15:38:38 -0300

    fix: update odontoprev-parceiro option value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,7 +169,7 @@ export default function Home() {
             <option value="nissan">Nissan</option>
             <option value="rod">ROD</option>
             <option value="plano-pet">Plano Pet</option>
-            <option value="odontoprev">Odontoprev Parceiro</option>
+            <option value="odontoprev-parceiro">Odontoprev Parceiro</option>
             <option value="odontoprev-site">Odontoprev Site</option>
             <option value="alugueldecarroai">Aluguel de Carro AI</option>
             <option value="unidas-seminovos">Unidas Seminovos</option>
